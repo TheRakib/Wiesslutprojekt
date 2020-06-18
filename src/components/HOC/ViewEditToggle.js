efdcc8a7e599874136155eb525962640fb244c94
Rakib Shelah Ahmed <Rakibmedieinstitet@gmail.com>
2020-06-18 10:27:40 +0200

    hoc class component

--- src/components/HOC/ViewEditToggle.js
+++ src/components/HOC/ViewEditToggle.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
 import withToggle from "./withToggle";
 
-const ViewEditToggle = ( {toggle, toggleStatus, title }) => {
 
+ //const ViewEditToggle= (props)=> {props.toggle}
+//const ViewEditToggle = ( {toggle, toggleStatus, title }) => {
 
+    class ViewEditToggle extends Component {
+
+   // hur vi läser props i class component??
+
+      // this.props.toggle 
+      // const { toggle , toggleStatus, title} = this.props;
+
+      
+      
+  
 //class component 
 //state onchange 
 //controlled element 
+render(){
+     const { toggle , toggleStatus, title} = this.props;
+
     return (
         <div>
              { toggleStatus ?
@@ -27,5 +41,8 @@ const ViewEditToggle = ( {toggle, toggleStatus, title }) => {
         </div>
     )
 }
+}
+
+
 
 export default   withToggle(ViewEditToggle);
